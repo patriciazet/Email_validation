1724c0594eaf99652fb13f3f3536f4f747bc234c
Patrycja Z <patrycja.zet3@gmail.com>
2022-07-30 21:50:47 +0200

    Initial commit

---
+++ Email_validation.cpp
@@ -0,0 +1,22 @@
+﻿// Wczytaj od uzytkownika lancuch znakow, a nastepnie sprawdz czy jest to poprawny adres e-mail.
+
+#include <iostream>
+#include <regex>
+
+bool checkEmail(const std::string& email)
+{
+    std::regex emailRegex("[a-z0-9_.]{3,}@[a-z0-9_]+.[a-z]{2,3}");
+    return std::regex_match(email, emailRegex);
+}
+
+int main()
+{
+    std::string email;
+    std::cout << "Please enter your e-mail address: " << std::endl;
+    std::cin >> email;
+
+    std::cout << email << " result: " << checkEmail(email) << std::endl;
+
+}
+
+
